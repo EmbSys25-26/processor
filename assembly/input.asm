35e1a4ecea426ab43bd3911c4f3266cb4f361c5d
diofilfrcos <diofilfrcos@gmail.com>
2026-03-05 21:33:28 +0000

    testbench_wdt

--- assembly/input.asm
+++ assembly/input.asm
@@ -15,7 +15,7 @@ reset:
     IMM   #0x440
     SW    a0, zero, #2    ; 4 to 0
 
-    ; Enable WDT with Interruot mode (Offset 0 -> 0x8800)
+    ; Enable WDT with Reset mode (Offset 0 -> 0x8800)
     ADDI  a0, zero, #3  ; Bit 0=1 (WDT Enable), Bit 1=1 (Reset Enable), Bit 2=0 (Interrupt Disable)
     IMM   #0x440
     SW    a0, zero, #0  
