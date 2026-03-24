d282bf7c8d6fdad9b80b13ec84004b57138386e8
aluisaCruz <analuisarcr0703@gmail.com>
2026-03-24 21:35:20 +0000

    4-stage pipeline code clean forwarding ifs

--- srcs/m_forwarding.v
+++ srcs/m_forwarding.v
@@ -13,37 +13,11 @@ module m_forwarding (
 	input wire i_validM,
 
     // Forwarding Multiplexer Control Outputs
-	output reg o_ForwardAE,  // Selector for ALU input A
-	output reg o_ForwardBE   // Selector for ALU input B
+	output wire o_ForwardAE,  // Selector for ALU input A
+	output wire o_ForwardBE   // Selector for ALU input B
 );
 
-    always @(*) begin
-        // -------------------------------------------------------------------------
-		// FORWARDING FOR OPERAND A (rdE)
-        // -------------------------------------------------------------------------
-        
-        // Priority 1: EX/MEM Hazard (The most recent data)
-        // If MEM stage is writing to our source register (and it is not r0)
-		if ((i_rdE != 4'h0) && (i_rdE == i_WriteRegM) && i_RegWriteM && i_validM) begin
-            o_ForwardAE = 1'b1;
-        end
-        // Normal Case: No hazards, use the value directly from the Register File
-        else begin
-            o_ForwardAE = 1'b0; 
-        end
-
-        // -------------------------------------------------------------------------
-		// FORWARDING FOR OPERAND B (rsE)
-        // -------------------------------------------------------------------------
-        
-        // Priority 1: EX/MEM Hazard
-		if ((i_rsE != 4'h0) && (i_rsE == i_WriteRegM) && i_RegWriteM && i_validM) begin
-            o_ForwardBE = 1'b1;
-        end
-        // Normal Case: No hazards
-        else begin
-            o_ForwardBE = 1'b0;
-        end
-    end
+assign o_ForwardAE = (i_rdE != 4'h0) && (i_rdE == i_WriteRegM) && i_RegWriteM && i_validM;
+assign o_ForwardBE = (i_rsE != 4'h0) && (i_rsE == i_WriteRegM) && i_RegWriteM && i_validM;
 
 endmodule
