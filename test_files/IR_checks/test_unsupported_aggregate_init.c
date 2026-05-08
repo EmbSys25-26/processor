09f2c63b279338f052e74ff623bf40592a47cdbf
blackmoney011 <rubencosta.fafe@gmail.com>
2026-05-08 11:53:58 +0100

    3 test files and change in parser

--- test_files/IR_checks/test_unsupported_aggregate_init.c
+++ test_files/IR_checks/test_unsupported_aggregate_init.c
@@ -1,4 +1,4 @@
 int main(void) {
-    int arr[3] = {1, 2, 3}; // Should reject this usage as aggregate initialization is not supported in this context
-    return arr[0]; 
+    int arr[] = {1, 2, 3}; // Should reject this usage as aggregate initialization is not supported in this context
+    return 0; 
 }
